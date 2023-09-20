ffd9a1c7e05b7bc5a3aa422a39b4222338a50d9a
marcin10lw <marcin10lw@wp.pl>
2023-09-20 20:36:37 +0200

    created dashboard layout

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,4 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import { Wrapper } from "src/assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "src/components";
+
 const DashboardLayout = () => {
-  return <h1>DashboardLayout</h1>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 export default DashboardLayout;
