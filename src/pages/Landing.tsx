e14a0fe411b0453e4028499769f7e2fc4ce07f28
marcin10lw <marcin10lw@wp.pl>
2023-09-20 16:37:05 +0200

    styled landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,41 @@
+import { Wrapper } from "src/assets/wrappers/LandingPage";
+import mainBackground from "src/assets/images/main.svg";
+import logo from "src/assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="Jobs application" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            officia alias assumenda molestiae. Laudantium impedit mollitia
+            animi, laboriosam ducimus distinctio ullam aliquam, molestiae
+            necessitatibus repellat porro obcaecati fuga eligendi libero, eum
+            quas numquam veritatis facere quidem ipsa similique? Id in dicta
+            maxime recusandae commodi. Ea omnis veritatis consequatur eligendi
+            ipsum.
+          </p>
+
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+
+        <img src={mainBackground} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 export default Landing;
