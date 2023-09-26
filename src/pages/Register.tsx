dc5a49ca88c9ccdd8f7577d3b136bf4d0697d2c0
marcin10lw <marcin10lw@wp.pl>
2023-09-26 13:46:34 +0200

    created register mutation

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,30 +1,49 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
+import customFetch from "src/utils/customFetch";
 import { Wrapper } from "src/assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormRow } from "src/components";
 import { RegisterFormData, registerFormData } from "src/models/Register";
+import { useState } from "react";
 
 const defaultValues = {
-  name: "john",
-  lastName: "doe",
-  location: "usa",
-  email: "john_doe@gamil.com",
-  password: "secret123",
+  name: "",
+  lastName: "",
+  location: "",
+  email: "",
+  password: "",
 };
 
 const Register = () => {
+  const [error, setError] = useState<AxiosError | undefined>(undefined);
+
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<RegisterFormData>({
     defaultValues,
     resolver: zodResolver(registerFormData),
   });
 
-  const onFormSubmit = (formData: RegisterFormData) => {};
+  const mutation = useMutation({
+    mutationFn: (formData: RegisterFormData) => {
+      return customFetch.post("/auth/register", formData);
+    },
+    onSuccess: () => reset(),
+    onError: (error: AxiosError<{ msg: string | undefined }>) => {
+      setError(error);
+    },
+  });
+
+  const onFormSubmit = (formData: RegisterFormData) => {
+    mutation.mutate(formData);
+  };
 
   return (
     <Wrapper>
@@ -68,7 +87,11 @@ const Register = () => {
           type="password"
         />
 
-        <button type="submit" className="btn btn-block">
+        <button
+          disabled={mutation.isLoading}
+          type="submit"
+          className="btn btn-block"
+        >
           Submit
         </button>
 
@@ -78,6 +101,17 @@ const Register = () => {
             Login
           </Link>
         </p>
+
+        {mutation.isSuccess && (
+          <p className="status-msg success-msg">User Created!</p>
+        )}
+
+        {mutation.isError && error && (
+          <p className="status-msg error-msg">
+            {error.response?.status === 400 && "invalid credentials"}
+            {error.response?.status === 404 && "could not register"}
+          </p>
+        )}
       </form>
     </Wrapper>
   );
