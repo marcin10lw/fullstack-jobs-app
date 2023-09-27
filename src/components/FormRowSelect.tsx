6c00e280d7d61d1072b060b14750700654a901d1
marcin10lw <marcin10lw@wp.pl>
2023-09-27 19:55:44 +0200

    created FormRowSelect

---
+++ src/components/FormRowSelect.tsx
@@ -0,0 +1,41 @@
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+type FormRowSelectProps = {
+  name: string;
+  labelText: string;
+  error: FieldError | undefined;
+  register: UseFormRegisterReturn;
+  defaultValue: string;
+  options: readonly string[];
+};
+
+const FormRowSelect = ({
+  name,
+  labelText,
+  register,
+  error,
+  defaultValue = "",
+  options,
+}: FormRowSelectProps) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText}
+      </label>
+      <select
+        {...register}
+        id={name}
+        className="form-select"
+        defaultValue={defaultValue}
+      >
+        {options.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FormRowSelect;
