c1ea4955c5f874b1f24bce79acf46de1ef902118
marcin10lw <marcin10lw@wp.pl>
2023-09-27 16:21:30 +0200

    queried current user

--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,25 +1,49 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
-import DashboardProvider from "src/context/DashboardContext";
+import { User } from "src/types";
+import customFetch from "src/utils/customFetch";
 import { Wrapper } from "src/assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "src/components";
+import DashboardProvider from "src/context/DashboardContext";
 
 const DashboardLayout = () => {
-  return (
-    <DashboardProvider>
-      <Wrapper>
-        <main className="dashboard">
-          <SmallSidebar />
-          <BigSidebar />
-          <div>
-            <Navbar />
-            <div className="dashboard-page">
-              <Outlet />
+  const {
+    data: user,
+    isError,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async (): Promise<User> => {
+      const { data } = await customFetch.get("/users/current-user", {
+        withCredentials: true,
+      });
+
+      return data.user;
+    },
+    retry: false,
+  });
+
+  if (isError) return <Navigate to="/login" />;
+
+  if (isSuccess) {
+    return (
+      <DashboardProvider>
+        <Wrapper>
+          <main className="dashboard">
+            <SmallSidebar />
+            <BigSidebar />
+            <div>
+              <Navbar user={user} />
+
+              <div className="dashboard-page">
+                <Outlet />
+              </div>
             </div>
-          </div>
-        </main>
-      </Wrapper>
-    </DashboardProvider>
-  );
+          </main>
+        </Wrapper>
+      </DashboardProvider>
+    );
+  }
 };
 export default DashboardLayout;
