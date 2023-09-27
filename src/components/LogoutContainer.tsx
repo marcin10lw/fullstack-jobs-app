52109e1dde345056d60e713b20d072a633c3e389
marcin10lw <marcin10lw@wp.pl>
2023-09-27 17:01:55 +0200

    setup logout request/logic

--- src/components/LogoutContainer.tsx
+++ src/components/LogoutContainer.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaCaretDown, FaUserCircle } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 import { User } from "src/types";
+import customFetch from "src/utils/customFetch";
 import useOutsideClick from "src/hooks/useOutsideClick";
 import { Wrapper } from "src/assets/wrappers/LogoutContainer";
 
@@ -12,6 +15,22 @@ type LogoutContainerProps = {
 const LogoutContainer = ({ user }: LogoutContainerProps) => {
   const [showLogout, setShowLogout] = useState(false);
   const ref = useOutsideClick<HTMLDivElement>(() => setShowLogout(false));
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const logoutUser = async () => {
+    setIsLoading(true);
+
+    try {
+      await customFetch.get("/auth/logout");
+      navigate("/login");
+      toast.success("Logout successfully", { position: "top-center" });
+      setIsLoading(false);
+    } catch (error) {
+      toast.error("Could not logout");
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Wrapper ref={ref}>
@@ -27,7 +46,7 @@ const LogoutContainer = ({ user }: LogoutContainerProps) => {
 
       {showLogout && (
         <div className="dropdown">
-          <button type="button" className="dropdown-btn">
+          <button onClick={logoutUser} type="button" className="dropdown-btn">
             logout
           </button>
         </div>
