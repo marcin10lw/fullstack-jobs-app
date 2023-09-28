7f5e862c9ec24f11877e87c92d361ccc435c8e95
marcin10lw <marcin10lw@wp.pl>
2023-09-28 16:22:02 +0200

    create fetch app stats logic

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,37 @@
+import { useLayoutEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import customFetch from "src/utils/customFetch";
+
 const Admin = () => {
-  return <h1>Admin</h1>;
+  const navigate = useNavigate();
+
+  const {
+    data: appStats,
+    status,
+    isError,
+  } = useQuery({
+    queryKey: ["app-stats"],
+    queryFn: () => customFetch.get("/users/admin/app-stats"),
+    retry: 0,
+  });
+
+  useLayoutEffect(() => {
+    if (isError) {
+      toast.error("You're not authorized to view this page", {
+        position: "top-center",
+      });
+      navigate("/dashboard");
+    }
+  }, [isError]);
+
+  if (status === "success")
+    return (
+      <div>
+        <h1>admin page</h1>
+      </div>
+    );
 };
 export default Admin;
