1ce57e8f7b179034edf73fcb9bdf032f27377d2c
marcin10lw <marcin10lw@wp.pl>
2023-09-29 18:41:54 +0200

    fetched stats data

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,4 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { ChartsContainer, StatsContainer } from "src/components";
+import NotFound from "src/components/NotFound";
+
+import { ApiStatsResponse } from "src/types";
+import customFetch from "src/utils/customFetch";
+
 const Stats = () => {
-  return <h1>Stats</h1>;
+  const { data, status } = useQuery({
+    queryKey: ["stats"],
+    queryFn: async (): Promise<ApiStatsResponse> => {
+      const { data } = await customFetch.get(`/jobs/stats`);
+      return data;
+    },
+  });
+
+  if (status === "error") return <NotFound text="Could not get stats" />;
+
+  if (status === "success") {
+    const { defaultStats, monthlyApplications } = data;
+
+    return (
+      <>
+        <StatsContainer defaultStats={defaultStats} />
+        {monthlyApplications.length > 1 && (
+          <ChartsContainer monthlyApplications={monthlyApplications} />
+        )}
+      </>
+    );
+  }
 };
 export default Stats;
