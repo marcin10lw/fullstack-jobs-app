1a2d12576230de8304a9c9935c7f4ff371db289b
marcin10lw <marcin10lw@wp.pl>
2023-09-29 18:41:41 +0200

    created basic stats related components

---
+++ src/components/ChartsContainer.tsx
@@ -0,0 +1,11 @@
+import { MonthlyApplications } from "src/types";
+
+type ChartsContainerProps = {
+  monthlyApplications: MonthlyApplications;
+};
+
+const ChartsContainer = ({ monthlyApplications }: ChartsContainerProps) => {
+  return <h1>ChartsContainer</h1>;
+};
+
+export default ChartsContainer;
---
+++ src/components/StatsContainer.tsx
@@ -0,0 +1,11 @@
+import { DefaultStats } from "src/types";
+
+type StatsContainerProps = {
+  defaultStats: DefaultStats;
+};
+
+const StatsContainer = ({ defaultStats }: StatsContainerProps) => {
+  return <h1>StatsContainer</h1>;
+};
+
+export default StatsContainer;
