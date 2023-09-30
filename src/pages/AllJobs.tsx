f00343b204daa9ef7355b7221d3d2a79f0a2cedb
marcin10lw <marcin10lw@wp.pl>
2023-09-30 13:27:04 +0200

    refetched jobs on searchParams change

--- src/pages/AllJobs.tsx
+++ src/pages/AllJobs.tsx
@@ -1,23 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 
 import customFetch from "src/utils/customFetch";
 import { Job } from "src/models/Job";
 import SearchContainer from "src/components/SearchContainer";
 import JobsContainer from "src/components/JobsContainer";
+import { useEffect } from "react";
 
 const AllJobs = () => {
+  const [searchParams] = useSearchParams();
+
   const {
     data: jobs,
     isLoading,
     isSuccess,
+    refetch,
   } = useQuery({
     queryKey: ["jobs"],
     queryFn: async (): Promise<Job[]> => {
-      const { data } = await customFetch.get("/jobs");
+      const { data } = await customFetch.get("/jobs", {
+        params: searchParams,
+      });
       return data.jobs;
     },
     keepPreviousData: true,
+    cacheTime: 0,
   });
+
+  useEffect(() => {
+    refetch();
+  }, [searchParams]);
 
   if (isLoading) return <h4>Loading...</h4>;
 
