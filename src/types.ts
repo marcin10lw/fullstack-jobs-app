c8321485e2d9a09f2860bfa032e42d10165b7a28
marcin10lw <marcin10lw@wp.pl>
2023-09-30 13:25:14 +0200

    created SearchOnChange type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import { AxiosError } from "axios";
 
+export type SearchOnChange = React.ChangeEvent<
+  HTMLInputElement | HTMLSelectElement
+>;
+
 export type Theme = "light" | "dark";
 
 export type CustomAxiosError = AxiosError<{ msg?: string }>;
