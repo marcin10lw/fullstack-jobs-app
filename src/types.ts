f1bc83dd704a9fff6eea3e92a45e04ebbe9c7860
marcin10lw <marcin10lw@wp.pl>
2023-10-02 14:48:01 +0200

    created SearchParamsObject

--- src/types.ts
+++ src/types.ts
@@ -37,3 +37,10 @@ export type ApiStatsResponse = {
   defaultStats: DefaultStats;
   monthlyApplications: MonthlyApplications;
 };
+
+export type SearchParamsObject = {
+  search?: string;
+  jobStatus?: "all" | "pending" | "interview" | "declined";
+  jobType?: "all" | "full-time" | "part-time" | "internship";
+  sort?: "newest" | "oldest" | "a-z" | "z-a";
+};
