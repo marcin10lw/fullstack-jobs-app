71fee59f5a7e3d4e97a9f73e97b0e9c75559dd14
marcin10lw <marcin10lw@wp.pl>
2023-10-03 21:01:05 +0200

    configured gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:3000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  base: "https://marcin10lw.github.io/fullstack-jobs-app",
   resolve: {
     alias: {
       src: "/src",
