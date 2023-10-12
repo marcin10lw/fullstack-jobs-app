847ba57f2aa5030d6f50e45de80d636b396d56f0
marcin10lw <marcin10lw@wp.pl>
2023-10-12 17:45:30 +0200

    changed axios baseURL to render

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  // baseURL: "https://jobs-api-dyvv.onrender.com/api/v1",
-  baseURL: "https://jobs-app-api-production.up.railway.app/api/v1",
+  baseURL: "https://jobs-api-dyvv.onrender.com/api/v1",
+  // baseURL: "https://jobs-app-api-production.up.railway.app/api/v1",
   // baseURL: "http://localhost:3000/api/v1",
   withCredentials: true,
 });
