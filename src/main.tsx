eb88d09e1169d568a8c35ef601f00c2c47cbe097
marcin10lw <marcin10lw@wp.pl>
2024-02-18 22:50:44 +0100

    feat(ui): installed toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 import { ToastContainer } from 'react-toastify';
+import { Toaster } from './components/ui/toaster';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
@@ -14,6 +16,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster />
+
+      {/* TO BE REPLACED BY SHADCN SONNER */}
       <ToastContainer
         position="bottom-center"
         limit={3}
