af5caf8c5a158679cbab0830ac21cd8e622f2440
marcin10lw <marcin10lw@wp.pl>
2024-02-27 00:01:15 +0100

    refactor(job): extracted update job logic to custom hook

--- src/views/dashboard/allJobs/allJobsList/jobItem/EditJobForm.tsx
+++ src/views/dashboard/allJobs/allJobsList/jobItem/EditJobForm.tsx
@@ -1,94 +1,30 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
-import { useForm } from 'react-hook-form';
 
 import LabeledFormSelect from 'src/components/LabeledFormSelect';
 import LabeledRegisterInput from 'src/components/LabeledRegisterInput';
 import MaxWidthWrapper from 'src/components/MaxWidthWrapper';
 import { Button } from 'src/components/ui/button';
 import { Form, FormField } from 'src/components/ui/form';
-import { useToast } from 'src/components/ui/use-toast';
-import errorMessage from 'src/lib/helpers/errorMessage';
-import {
-  ALL_JOBS_QUERY_KEY,
-  JOB_BY_ID_QUERY_KEY,
-} from 'src/infrasctucture/job/constants';
-import { jobAPI } from 'src/infrasctucture/job/jobApiAdapter';
-import {
-  InferJob,
-  Job as JobType,
-  jobSchema,
-  jobStatusItems,
-  jobTypeItems,
-} from 'src/models/Job';
-import { CustomAxiosError } from 'src/types';
-import { checkHasAnyFieldChanged } from 'src/lib/helpers/checkHasAnyFieldChanged';
+import { Job as JobType, jobStatusItems, jobTypeItems } from 'src/models/Job';
+import { useUpdateJob } from './useUpdateJob';
 
-type EditJobFormProps = {
+interface EditJobFormProps {
   job: JobType;
   jobId: string;
   closeDrawer: () => void;
-};
+}
 
 const EditJobForm = ({ job, jobId, closeDrawer }: EditJobFormProps) => {
-  const qc = useQueryClient();
-
-  const { toast } = useToast();
-
-  const initialJob = {
-    position: job.position,
-    company: job.company,
-    jobLocation: job.jobLocation,
-    jobStatus: job.jobStatus,
-    jobType: job.jobType,
-  };
-
-  const form = useForm<InferJob>({
-    defaultValues: initialJob,
-    resolver: zodResolver(jobSchema),
-  });
-
   const {
     register,
-    formState: { errors },
     handleSubmit,
     control,
-    watch,
-  } = form;
-
-  const currentFormValues = watch();
-  const hasAnyFieldChanged = checkHasAnyFieldChanged(
-    currentFormValues,
-    initialJob,
-  );
-
-  const { mutate: updateJob, isLoading: isJobUpdating } = useMutation({
-    mutationFn: (job: InferJob) => jobAPI.updateJobById(job, jobId),
-    onSuccess: () => {
-      qc.invalidateQueries([ALL_JOBS_QUERY_KEY]);
-      qc.invalidateQueries([JOB_BY_ID_QUERY_KEY]);
-      closeDrawer();
-      setTimeout(() => {
-        toast({
-          title: 'Job updated successfully',
-          variant: 'success',
-        });
-      }, 300);
-    },
-    onError: (error: CustomAxiosError) => {
-      toast({
-        title: 'An error occurred',
-        description: errorMessage(error, 'Failed to update job'),
-        variant: 'destructive',
-      });
-    },
-  });
-
-  const onFormSubmit = (job: InferJob) => {
-    if (!hasAnyFieldChanged) return;
-    updateJob(job);
-  };
+    errors,
+    hasAnyFieldChanged,
+    isJobUpdating,
+    onFormSubmit,
+    form,
+  } = useUpdateJob({ job, jobId, closeDrawer });
 
   return (
     <MaxWidthWrapper>
---
+++ src/views/dashboard/allJobs/allJobsList/jobItem/useUpdateJob.ts
@@ -0,0 +1,91 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+
+import { useToast } from 'src/components/ui/use-toast';
+import {
+  ALL_JOBS_QUERY_KEY,
+  JOB_BY_ID_QUERY_KEY,
+} from 'src/infrasctucture/job/constants';
+import { jobAPI } from 'src/infrasctucture/job/jobApiAdapter';
+import { checkHasAnyFieldChanged } from 'src/lib/helpers/checkHasAnyFieldChanged';
+import errorMessage from 'src/lib/helpers/errorMessage';
+import { InferJob, Job, jobSchema } from 'src/models/Job';
+import { CustomAxiosError } from 'src/types';
+
+interface Props {
+  job: Job;
+  jobId: string;
+  closeDrawer: () => void;
+}
+
+export const useUpdateJob = ({ job, jobId, closeDrawer }: Props) => {
+  const qc = useQueryClient();
+
+  const { toast } = useToast();
+
+  const initialJob = {
+    position: job.position,
+    company: job.company,
+    jobLocation: job.jobLocation,
+    jobStatus: job.jobStatus,
+    jobType: job.jobType,
+  };
+
+  const form = useForm<InferJob>({
+    defaultValues: initialJob,
+    resolver: zodResolver(jobSchema),
+  });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    control,
+    watch,
+  } = form;
+
+  const currentFormValues = watch();
+  const hasAnyFieldChanged = checkHasAnyFieldChanged(
+    currentFormValues,
+    initialJob,
+  );
+
+  const { mutate: updateJob, isLoading: isJobUpdating } = useMutation({
+    mutationFn: (job: InferJob) => jobAPI.updateJobById(job, jobId),
+    onSuccess: () => {
+      qc.invalidateQueries([ALL_JOBS_QUERY_KEY]);
+      qc.invalidateQueries([JOB_BY_ID_QUERY_KEY]);
+      closeDrawer();
+      setTimeout(() => {
+        toast({
+          title: 'Job updated successfully',
+          variant: 'success',
+        });
+      }, 300);
+    },
+    onError: (error: CustomAxiosError) => {
+      toast({
+        title: 'An error occurred',
+        description: errorMessage(error, 'Failed to update job'),
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const onFormSubmit = (job: InferJob) => {
+    if (!hasAnyFieldChanged) return;
+    updateJob(job);
+  };
+
+  return {
+    register,
+    errors,
+    handleSubmit,
+    control,
+    isJobUpdating,
+    hasAnyFieldChanged,
+    onFormSubmit,
+    form,
+  };
+};
